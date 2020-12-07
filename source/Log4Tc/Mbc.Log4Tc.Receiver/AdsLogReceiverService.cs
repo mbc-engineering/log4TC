32173cbd20d28fd8fdf2347ad2d3aca89f0c9f3d
Stefan Gmeiner <stefan.gmeiner@mbc-engineering.ch>
2020-12-07 13:41:35 +0100

    Bugfix start timeout. (#219)

--- source/Log4Tc/Mbc.Log4Tc.Receiver/AdsLogReceiverService.cs
+++ source/Log4Tc/Mbc.Log4Tc.Receiver/AdsLogReceiverService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,10 +8,14 @@ namespace Mbc.Log4Tc.Receiver
 {
     public class AdsLogReceiverService : IHostedService, IDisposable
     {
+        private readonly CancellationTokenSource _stopToken = new CancellationTokenSource();
+        private readonly ILogger<AdsLogReceiver> _logger;
         private readonly AdsLogReceiver _adsLogReceiver;
+        private Task _connectTask;
 
-        public AdsLogReceiverService(AdsLogReceiver adsLogReceiver)
+        public AdsLogReceiverService(ILogger<AdsLogReceiver> logger, AdsLogReceiver adsLogReceiver)
         {
+            _logger = logger;
             _adsLogReceiver = adsLogReceiver;
         }
 
@@ -21,14 +26,46 @@ namespace Mbc.Log4Tc.Receiver
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _adsLogReceiver.Connect();
+            _connectTask = Task.Run(() => ConnectAsync(_stopToken.Token));
             return Task.CompletedTask;
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        public async Task StopAsync(CancellationToken cancellationToken)
         {
+            _stopToken.Cancel();
+            if (_connectTask != null && !_connectTask.IsCompleted)
+            {
+                try
+                {
+                    await _connectTask;
+                }
+                catch (Exception)
+                {
+                    // ignored - we just want to ensure no task is running
+                }
+            }
+
             _adsLogReceiver.Disconnect();
-            return Task.CompletedTask;
+        }
+
+        private async Task ConnectAsync(CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    _adsLogReceiver.Connect();
+                    _logger.LogInformation("Log receiver connected.");
+                    return;
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Error connecting log receiver. Waiting for next retry.");
+                }
+
+                // Wait for next connect
+                await Task.Delay(TimeSpan.FromSeconds(30), ct);
+            }
         }
     }
 }
