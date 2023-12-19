af74e12921e8dd30d377d08de8aba7d735d04515
Michael Helfenstein <michael.helfenstein@noser.com>
2023-12-19 14:39:09 +0100

    #17 - Add ApplicationInsight Reference for nlog Output

--- source/Log4Tc/Mbc.Log4Tc.Output.NLog/NLogLog4TcOutputPlugin.cs
+++ source/Log4Tc/Mbc.Log4Tc.Output.NLog/NLogLog4TcOutputPlugin.cs
@@ -9,6 +9,10 @@ namespace Mbc.Log4Tc.Output.NLog
         public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IOutputFactory, NLogLog4TcOutputFactory>();
+
+            // Example see: https://blog.skrots.com/nlog-implementation-with-azure-application-insights/?feed_id=1833&_unique_id=64f62b11bebcc
+            // See Application Insights logging adapters: https://github.com/microsoft/ApplicationInsights-dotnet/blob/c420e04562876791f27a61644760d7b7512832d9/LOGGING/README.md#nlog
+            services.AddApplicationInsightsTelemetryWorkerService();
         }
     }
 }
